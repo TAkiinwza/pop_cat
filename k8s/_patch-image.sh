b6af119272e05a95ebc5b212ca0a617ee6a6f9f3
K1M <zkim2547@gmail.com>
2025-08-24 15:10:43 +0700

    Delete k8s directory

--- k8s/_patch-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-ns=popcat
-sha=${1:?need short sha}
-kubectl -n $ns set image deploy/popcat-backend app=ghcr.io/OWNER/popcat-backend:$sha
-kubectl -n $ns set image deploy/popcat-frontend web=ghcr.io/OWNER/popcat-frontend:$sha
